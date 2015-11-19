b9b796eef9db36659f2556121f448bca65626b2f
jcg678 <jcg678@hotmail.es>
2015-11-20 00:16:21 +0100

    Cuerpo Html y carpeta css

--- main.php
+++ main.php
@@ -1,3 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>BINGO</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+    
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -44,3 +53,11 @@ echo "<tr>";
 
 
 echo "</table>";
+?>
+<form action="main.php" method="post">
+<input type="submit" name="numero" value="Obtener Número"/>
+<input type="submit" name="nuevo" value="Juego Nuevo"/>    
+</body>
+</html>
+
+
