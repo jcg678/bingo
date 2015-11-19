576b8269cd398241a8ca7ed49d4244dd0b59ea37
jcg678 <jcg678@hotmail.es>
2015-11-19 23:28:31 +0100

    terminado apartado A

--- main.php
+++ main.php
@@ -5,26 +5,38 @@
  * Date: 18/11/15
  * Time: 13:22
  */
+ 
+echo "BINGO";
 echo "<table border='3'>";
-
-//echo "<tr><td>X</td><td>10</td><td>20</td><td>30</td><td>40</td><td>50</td><td>60</td><td>70</td><td>80</td></tr>";
-
-//for($i=1 ;$i<=9 ;$i++){
-//    echo "<tr><td>".$i."</td><td>".(10+$i)."</td><td>".(20+$i)."</td><td>".(30+$i)."</td><td>".(40+$i)."</td><td>".(50+$i)."</td><td>".(60+$i)."</td><td>".(70+$i)."</td><td>".(80+$i)."</td></tr>";
-//}
-//echo "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>90</td></tr>";
-
 for($i=0 ;$i<=10 ;$i++){
 echo "<tr>";
 
     for($x=1 ;$x<=8 ;$x++){
+		$res=($x * 10 + $i);
+		
+        if($x==1){
+			if($i==0 or $i==10){
+				echo "<td></td>";
+			}
+			else{
+				echo "<td>" . $i . "</td>";
+			}
+		}
+			
+				
+			
+		
+			if($i==10){
+				if($x==8){
+					
+					echo "<td>" . $res . "</td>";
 
-        if($x==1)echo "<td>".$i."</td>";
-
-
-            echo "<td>" . ($x * 10 + $i) . "</td>";
-
-
+				}
+				else
+			echo "<td></td>";
+			}
+			else
+			echo "<td>" . $res . "</td>";
 
     }
     echo "</tr>";
